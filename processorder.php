be1f7c74802569361a67855f56b062e40b60dd68
Shimazaky80 <64324142+Shimazaky80@users.noreply.github.com>
2021-04-16 20:52:52 +0200

    updated orderform.html and processorder.php
    orderform.html:
    - Added reset button
    - Cleared CSS
    
    processorder.php:
    - Copied form field names as variables using $_POST
    - Used htmlspecialchars for safer form field entries
    - Cleaner output on client
    - Created $totalqty and $totalamount variables for future numbers of items sold and the total amount respectively
    - Created constant variables for items on sale

--- processorder.php
+++ processorder.php
@@ -11,11 +11,45 @@
 
 	<?php  
 
-		echo '<p>Order processed successfully!</p>';
-		echo '<p>At ';
-		echo date('H:i, jS F Y');
-		echo '</p>';
+		# creating short variable names
+		# from form names via $_POST global variable
+		$tireqty = $_POST['tireqty'];
+		$oilqty = $_POST['oilqty'];
+		$sparkqty = $_POST['sparkqty'];
+
+		echo '<p>Your order is as follows: </p>';
+		$tireqty = htmlspecialchars($tireqty);
+		$oilqty = htmlspecialchars($oilqty);
+		$sparkqty = htmlspecialchars($sparkqty);
+
+		echo <<<theEnd
+
+			$tireqty tires<br /> 
+			$oilqty bottles of oil<br />
+			$sparkqty spark plugs<br />
+
+			theEnd;
+
+		/* a more condensed way, but hard to read and prone to error
+		echo htmlspecialchars($tireqty) . ' <span>tires</span><br />';
+		echo htmlspecialchars($oilqty) . ' <spa>bottles of oil</span><br />';
+		echo htmlspecialchars($sparkqty) . ' <span>spark plugs</span><br />';
+		*/
+
+		// price for each item on sale
+		define('TIREPRICE', 100);
+		define('OILPRICE', 10);
+		define('SPARKPRICE', 4);
+
+		$totalqty = 0;
+		$totalamount = 0.00;
+
+		echo '<p>Order processed successfully at ' . date('H:i, jS F Y') . '</p>';
 
 	?>
+
+	<p>
+		<a href="http://localhost/training/orderform.html">Home</a>
+	</p>
 </body>
 </html>
